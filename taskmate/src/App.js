8175f798d47ff20331b78ef5e1de540f86259fa0
alaminsun <sunmoon843@gmail.com>
2024-02-13 17:21:08 +0600

    first

---
+++ taskmate/src/App.js
@@ -0,0 +1,36 @@
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import './App.css';
+
+export default function App() {
+  
+  return (
+    <>
+    <Header />
+    <div>
+      <h1 className='active'>alamin</h1>
+    </div>
+    <Footer/>
+    </>
+  )
+}
+
+
+
+ 
+ 
+ 
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
